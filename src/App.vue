e70934f1a0a0f78f2ea00069abbb9714aa37fe06
牛亮 <nll@itcast.cn>
2019-07-08 09:08:24 +0800

    实现AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">CRUD</a>
-        </div>
-      </div>
-    </nav>
+    <!-- AppHeader -->
+    <app-header></app-header>
+
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
@@ -241,13 +231,14 @@
 </template>
 
 <script>
-
+// 导入AppHeader组件
+import AppHeader from './components/AppHeader.vue'
 
 export default {
   name: 'app',
   // 注册组件
   components: {
-    
+    AppHeader
   }
 }
 </script>
