6eb6a3bc19fcd694a6e6cc394d40b137237abd0a
牛亮 <nll@itcast.cn>
2019-07-08 08:51:22 +0800

    删除示例代码

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- 使用组件 -->
-    <hero-list></hero-list>
+    
   </div>
 </template>
 
 <script>
-// 导入组件
-import HeroList from './views/HeroList.vue'
+
 
 export default {
   name: 'app',
   // 注册组件
   components: {
-    HeroList
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
