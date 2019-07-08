15ee365e5b41febba70874ee840e1c6387d92dda
牛亮 <nll@itcast.cn>
2019-07-08 11:28:16 +0800

    实现添加功能

--- src/views/heroes/HeroAdd.vue
+++ src/views/heroes/HeroAdd.vue
@@ -4,23 +4,57 @@
     <form>
       <div class="form-group">
         <label for="txtName">姓名</label>
-        <input type="text" class="form-control" id="txtName" placeholder="姓名">
+        <input type="text" class="form-control" v-model="formData.name" id="txtName" placeholder="姓名">
       </div>
       <div class="form-group">
         <label for="sex">性别</label>
-        <select class="form-control">
+        <select class="form-control" v-model="formData.gender">
           <option value="男">男</option>
           <option value="女">女</option>
         </select>
       </div>
-      <button type="submit" class="btn btn-success">提交</button>
+      <!-- prevent 阻止默认行为的执行 -->
+      <button @click.prevent="add" class="btn btn-success">提交</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  data() {
+    return {
+      // 1. 绑定文本框
+      formData: {
+        name: '',
+        gender: '男'
+      }
+    }
+  },
+  methods: {
+    // 2. 点击提交按钮，添加英雄
+    add() {
+      axios
+        .post('http://localhost:3000/heroes', this.formData)
+        .then((response) => {
+          const status = response.status
+          // 判断添加成功还是失败
+          if (status === 201) {
+            // 添加成功
+            // 3. 跳转到列表组件
+            this.$router.push({ name: 'heroes' })
+          } else {
+            // 添加失败
+            alert('添加失败')
+          }
+        })
+        .catch((err) => {
+          alert('服务器异常' + err)
+        })
+      // return false
+    }
+  }
 }
 </script>
 
